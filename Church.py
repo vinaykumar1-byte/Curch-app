953a84bdbf8f02996157aed8ff019a78e289aa31
vinaykumar1-byte <vinay.kumar1@bizloanindia.com>
2026-05-31 10:48:34 +0530

    Church.py

--- Church.py
+++ Church.py
@@ -50,7 +50,7 @@ col1, col2, col3, col4, col5, col6 = st.columns(6)
 
 with col1:
     if st.button("📝 DATA ENTRY "):
-        st.switch_page("1_Data_Entry.py")
+        st.switch_page("pages/1_Data_Entry.py")
 
 with col2:
     if st.button("🎂  BIRTHDAY  "):
